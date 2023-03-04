f9590255837a8b550d7f017ba61771dfbc2a6e5d
ColeANiblett <coleaniblett@gmail.com>
2023-03-04 15:26:25 -0800

    add hero background image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import AmyGuitar from '../AmyGuitar.gif';
 
 export const Hero = () => {
   return (
-    <div className="row bg-primary">
+    <div className="row min-vh-100" 
+      style={{
+        backgroundImage: `url(${AmyGuitar})`, 
+        backgroundSize: "cover", 
+        backgroundPosition: "center", 
+        backgroundRepeat: "no-repeat"
+      }}
+    >
       <p className="text-white text-center">
         Hero
       </p>
