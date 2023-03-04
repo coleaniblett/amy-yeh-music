ddfb5535c8be90dba2d77976db4e19bdfbbca04b
ColeANiblett <coleaniblett@gmail.com>
2023-03-04 15:26:08 -0800

    setup project structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <div className="container min-vh-100">
+      <div className="container-fluid">
         <Hero />
         <Bio />
         <Music />
