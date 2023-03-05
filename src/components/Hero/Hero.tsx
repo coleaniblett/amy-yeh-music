6de7f5b98790e63b8fc1c75095d984f7306f97b0
ColeANiblett <coleaniblett@gmail.com>
2023-03-05 15:36:02 -0800

    add hero text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,12 +8,17 @@ export const Hero = () => {
         backgroundImage: `url(${AmyGuitar})`, 
         backgroundSize: "cover", 
         backgroundPosition: "center", 
-        backgroundRepeat: "no-repeat"
+        backgroundRepeat: "no-repeat",
       }}
     >
-      <p className="text-white text-center">
-        Hero
-      </p>
+      <div className="position-absolute top-50 text-white">
+        <h1 className="display-1">
+          Tzu-chi "Amy" Yeh
+        </h1>
+        <h3 className="display-6">
+          Keyboardist, guitarist, composer
+        </h3>
+      </div>
     </div>
   );
 }
