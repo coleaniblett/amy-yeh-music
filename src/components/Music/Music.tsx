1f002432891f48f8b7af1876c2c8135678e35366
ColeANiblett <coleaniblett@gmail.com>
2023-03-05 22:43:04 -0800

    add placeholder soundcloud tracks

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 export const Music = () => {
   return (
-    <div className="row bg-primary">
-      <p className="text-white text-center">
+    <div className="row bg-black text-white">
+      <h3>
         Music
-      </p>
+      </h3>
+      <iframe  
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1305290209&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      >
+      </iframe>
+      <iframe  
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1305290209&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      >
+      </iframe>
+      <iframe  
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1305290209&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      >
+      </iframe>
     </div>
   );
 }
