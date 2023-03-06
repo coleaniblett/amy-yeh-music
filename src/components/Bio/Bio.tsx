3fb86aeba7101a3aa9437f27343b57c722e18d95
ColeANiblett <coleaniblett@gmail.com>
2023-03-05 22:42:42 -0800

    add placeholder text

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 export const Bio = () => {
   return (
-    <div className="row bg-secondary">
-      <p className="text-white text-center">
-        Bio
-      </p>
+    <div className="row bg-black">
+      <div className="text-white p-4">
+        <h3>
+          Bio
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+        Ut tempus eleifend ex nec pharetra. Mauris a dictum nisi. Donec quis elit mollis, egestas tortor rhoncus, condimentum mi. Nulla non nulla pulvinar, feugiat est vitae, lacinia risus. Nam eleifend eleifend ipsum ac tempor. Donec tempus pellentesque erat non posuere. Vestibulum sed eleifend tellus. Morbi ligula leo, vehicula id justo sagittis, congue sollicitudin libero.
+        </p>
+      </div>
     </div>
   );
 }
