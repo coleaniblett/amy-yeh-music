ec7b0970ab35029dadb14d76c04cdb69a8714a62
ColeANiblett <coleaniblett@gmail.com>
2023-03-26 20:13:30 -0700

    adjust fonts

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Contact.css';
 
 export const Contact: React.FC = () => {
   return (
     <div className="contact d-flex p-3 text-white bg-black text-center">
-      <p className="p-2 col">Email</p>
-      <p className="p-2 col">Soundcloud</p>
-      <p className="p-2 col">Phone</p>
+      <ul>
+        <li className="p-2 col"><a href="mailto:amyyeh3391@gmail.com">Email</a></li>
+        <li className="p-2 col"><a href="https://soundcloud.com/search?q=amy%20yeh">Soundcloud</a></li>
+        <li className="p-2 col"><a href="tel:14083071203">Phone</a></li>
+      </ul>
     </div>
   );
 }
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 export const Hero: React.FC = () => {
   return (
-    <div className="hero position-relative min-vh-100 p-3">
+    <div className="hero position-relative min-vh-100 p-4">
       <div className="position-absolute top-50 text-white">
-        <h1 className="display-1 fw-normal">
+        <h1 className="display-1">
           Tzu-chi "Amy" Yeh
         </h1>
-        <h3 className="display-6">
+        <h3 className="display-6 m-3">
           Keyboardist, guitarist, composer
         </h3>
       </div>
