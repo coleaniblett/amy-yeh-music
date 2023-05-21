ec9b219a96e3c8c504f55abcb85f0b40db1e90a6
ColeANiblett <coleaniblett@gmail.com>
2023-05-21 13:01:13 -0700

    restore default controls

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './Video.css';
-import Play from '../../assets/Play.png';
-import Pause from '../../assets/Pause.png';
+
 
 interface Props {
   media: any
@@ -9,40 +8,6 @@ interface Props {
 
 export const Video: React.FC<Props> = ({media}: Props) => {
   const [hover, setHover] = useState(false);
-  const [play, setPlay] = useState(true);
-  const [volume, setVolume] = useState(50);
-
-  const videoRef = useRef(document.createElement("video"));
-
-  const handleClick = () => {
-    setPlay(!play);
-    if (play) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
-    }
-  }
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputElement = event.target as HTMLInputElement;
-    const volumeValue = parseInt(inputElement.value);
-    setVolume(volumeValue);
-    videoRef.current.volume = (volume / 100);
-    console.log(videoRef.current.volume);
-  }
-
-  const showControls = () => {
-    return (
-      <div className="controls" onMouseLeave={handleLeave}>
-        <button className="play" onClick={handleClick}>
-          <img src={play ? Play: Pause} />
-        </button>
-        <div className="slidecontainer">
-          <input type="range" min="-1" max="100" defaultValue={volume} className="slider" onChange={handleChange}/>
-        </div>
-      </div>
-    )
-  }
 
   const handleHover =() => {
     setHover(true);
@@ -53,13 +18,12 @@ export const Video: React.FC<Props> = ({media}: Props) => {
     setHover(false);
   }
 
-  return (
+return (
     <div className="video">
-      <video className="media" onMouseOver={handleHover} ref={videoRef}>
+      <video className="media" controls>
         <source src={media} type="video/mp4"></source>
         Your browser does not support the video tag.
       </video>
-      {hover && showControls()}
     </div>
   );
 }
