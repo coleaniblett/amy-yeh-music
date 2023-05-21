88e5f229e81ab58313f1b6dec1f381263579ad9b
ColeANiblett <coleaniblett@gmail.com>
2023-05-21 13:00:58 -0700

    remove Banner

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { Banner } from'../Banner/Banner';
 import { Hero } from '../Hero/Hero';
 import { Intro } from '../Intro/Intro';
 import { Highlights } from '../Highlights/Highlights';
@@ -8,16 +7,14 @@ import { Contact } from '../Contact/Contact';
 
 /*
   TO-DO
-  1. Add drop-down menu
-  2. Add links to Contact
-  4. Customize video controls
-  5. Font consistency
+  1. Fix clipping at low end of slider
+  2. Increase size of play/pause buttons to full body
+  3. Fix issue with onLeave removal of video controls not always working
 */
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Banner />
       <Hero />
       <div className="main">
         <Intro />
